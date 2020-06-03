5ba8d745cc70617c69430b65f8fe89b06a9c6e5a
Shrutika Patil <shrutika.patil@northgateps.com>
2020-06-04 02:42:28 +0530

    Session create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,24 @@ class SessionsController < ApplicationController
   def new
     puts 'you are on index page'
   end
+
+  def create
+    # byebug
+    # puts 'you are on index page'
+    user = User.find_by(username: params[:session][:username])
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'There is something wrong with provided info'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to login_path
+  end
 end
